887c11c08d30b8567d88341154bb0fe98c42ed15
Farah Tahnin <farahtahnin@Farahs-MacBook-Air.local>
2026-01-05 20:22:30 -0500

    render array of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import Header from "/src/components/Header";
 import Entry from "/src/components/Entry";
+import data from "./data";
 export default function App() {
+  const entryElements = data.map((entry) => {
+    return (
+      <Entry
+        id={entry.id}
+        img={entry.img}
+        title={entry.title}
+        dates={entry.dates}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        text={entry.text}
+      />
+    );
+  });
   return (
     <>
       <Header />
-      <Entry />
+      {entryElements}
     </>
-  )
+  );
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,31 +1,24 @@
 import Japan from "/src/assets/japan.png";
 import Marker from "/src/assets/marker.png";
-export default function Entry() {
+export default function Entry(props) {
   return (
     <main>
       <div className="entry-item">
         <div className="image-container">
-          <img src={Japan} alt="Japan" />
+          <img src={props.img.src} alt={props.img.alt} />
         </div>
         <div className="location-container">
           <div className="location">
             <img className="location-marker" src={Marker} alt="marker" />
-            <p className="location-country">JAPAN</p>
-            <a
-              className="location-googlemaplink"
-              href="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9"
-            >
+            <p className="location-country">{props.country}</p>
+            <a className="location-googlemaplink" href={props.googleMapsLink}>
               View on Google Maps
             </a>
           </div>
           <div className="location-details">
-            <h2 className="entry-location-title">Mount Fuji</h2>
-            <p className="entry-location-dates">12 Jan, 2023 - 24 Jan, 2023</p>
-            <p className="entry-location-description">
-              Mount Fuji is the tallest mountain in Japan, standing at 3,776
-              meters (12,380 feet). Mount Fuji is the single most popular
-              tourist site in Japan, for both Japanese and foreign tourists.
-            </p>
+            <h2 className="entry-location-title">{props.title}</h2>
+            <p className="entry-location-dates">{props.dates}</p>
+            <p className="entry-location-description">{props.text}</p>
           </div>
         </div>
       </div>
